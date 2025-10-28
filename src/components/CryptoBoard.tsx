e3ab09e7420f61868dc755dc5fb3a183cd4ea029
tvece <t.vecera97@gmail.com>
2025-10-28 04:55:08 +0100

    improve comments

--- src/components/CryptoBoard.tsx
+++ src/components/CryptoBoard.tsx
@@ -8,7 +8,6 @@ import { SearchForm, type FilterFormElement } from "./SearchForm";
 import ColumnHeaderCell from "./ColumnHeaderCell";
 
 /**
- * TODO: comments
  * TODO: try to fix sticky header on mobile
  * TODO: try to have transition
  * TODO: check websockets throttle implementation
@@ -38,13 +37,25 @@ const columns: ColumnDefinition[] = [
 type CryptoBoardProps = CoinsFeedConfig;
 
 export default function CryptoBoard({ monitoredCoinsCount, coinUpdateThrottle, highlightDuration }: CryptoBoardProps) {
-  const { coins, /*setCoins,*/ status } = useCoinsFeed({ monitoredCoinsCount, coinUpdateThrottle, highlightDuration });
+  // hooks
+  const { coins, status } = useCoinsFeed({ monitoredCoinsCount, coinUpdateThrottle, highlightDuration });
 
   const [filter, setFilter] = useState<string | undefined>();
   const inputFilterRef = useRef<HTMLInputElement>(null);
 
   const [isAscendingSort, setIsAscendingSort] = useState(true);
   const [sortKey, setSortKey] = useState<string>(columns[0].id);
+
+  // not ready early returns
+
+  if (status === COINS_FEED_STATUS.ERROR) {
+    return <h1 className="global-message">Načítání dat selhalo!</h1>;
+  }
+  if (status !== COINS_FEED_STATUS.READY) {
+    return <h1 className="global-message">Načítání...</h1>;
+  }
+
+  // events
 
   const handleFilter = (event: FormEvent<FilterFormElement>) => {
     event.preventDefault();
@@ -59,6 +70,9 @@ export default function CryptoBoard({ monitoredCoinsCount, coinUpdateThrottle, h
     setFilter(undefined);
   };
 
+  /**
+   * change sort on header cell click
+   */
   const columnHeaderCellClick = (accessorKey: string) => {
     if (sortKey === accessorKey) {
       if (isAscendingSort) {
@@ -74,14 +88,9 @@ export default function CryptoBoard({ monitoredCoinsCount, coinUpdateThrottle, h
     }
   };
 
-  if (status === COINS_FEED_STATUS.ERROR) {
-    return <h1 className="global-message">Načítání dat selhalo!</h1>;
-  }
-  if (status !== COINS_FEED_STATUS.READY) {
-    return <h1 className="global-message">Načítání...</h1>;
-  }
+  // aply sort and filter
 
-  const curatedCoins = getSortedAndFilteredCoins(coins, filter, sortKey, isAscendingSort);
+  const curatedCoins = getCuratedCoins(coins, filter, sortKey, isAscendingSort);
 
   return (
     <table className="cb">
@@ -129,7 +138,7 @@ export default function CryptoBoard({ monitoredCoinsCount, coinUpdateThrottle, h
 /**
  * @returns sorted and filtered coins
  */
-function getSortedAndFilteredCoins(
+function getCuratedCoins(
   allCoins: Coin[],
   filter: string | undefined,
   sortKey: string,
